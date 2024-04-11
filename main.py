5524be5d8616f5aed5447bfcad19b4b850eb2cb5
lduynkerke <lduynkerke@gmail.com>
2024-04-11 21:24:14 +0200

    Process data and calculate correlation table

--- main.py
+++ main.py
@@ -1,33 +1,36 @@
 import os
 import pandas as pd
 from pprint import pprint
-import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 def main():
     file_dir = "data/"
     files = sorted(get_files_recursively(f"{file_dir}"))
-    
+
     all_block_dfs = {}
     all_transaction_dfs = {}
+    all_price_dfs = {}
     for file in files:
         print(file)
         # date = file.split('/')[2][5:] # Linux
-        date = file.split('\\')[1][5:] # Windows
+        date = file.split('\\')[1][5:]  # Windows
         print(date)
         df = read_snappy_parquet(file)
         if "blocks" in file:
             all_block_dfs[date] = df
         if "transactions" in file:
             all_transaction_dfs[date] = df
-            
-    # pprint(all_block_dfs)
-    # pprint(all_transaction_dfs)
-    pprint(all_transaction_dfs['2024-03-01'].columns)
+        if "price" in file:
+            all_price_dfs[date] = df
 
-    G = create_network(all_transaction_dfs)
-    print_network_data(G)
+    pprint(all_block_dfs['2024-04-01'].columns)
+    pprint(all_price_dfs['2024-03-01'].columns)
+
+    # Analyse correlation
+    correlation_df = format_data(all_block_dfs, all_price_dfs)
+    analyse_correlation(correlation_df)
 
 
 def read_snappy_parquet(file_path):
@@ -47,36 +50,39 @@ def get_files_recursively(directory):
     return file_list
 
 
-def create_network(dict_df):
-    G = nx.DiGraph() # Directed graph since transactions are one way only
-    df = dict_df['2024-03-01']
+def format_data(block_dict, price_dict):
+    df_price = price_dict['2024-03-01']
+    df_price['date'] = pd.to_datetime(df_price['date'].dt.tz_localize(None))
+    df_price['volatility'] = np.log(df_price['close']).diff().rolling(window=30).std()
 
-    for i in range(df.shape[0]):
-        cell1 = df['inputs'].iloc[i]
-        cell2 = df['outputs'].iloc[i]
+    df_network = pd.concat(
+        [df[['timestamp', 'size', 'transaction_count']] for df in block_dict.values()],
+        ignore_index=True
+    )
+    df_network['timestamp'] = pd.to_datetime(df_network['timestamp'].dt.floor('T'))
 
-        if cell1 is not None and cell2 is not None:
-            address1 = cell1[0]['address']
-            address2 = cell2[0]['address']
+    df = pd.merge(df_network, df_price[['close', 'volume', 'date', 'volatility']],
+                  left_on='timestamp', right_on='date', how='inner')
+    df.drop('date', axis=1, inplace=True)
 
-            # Add nodes (BTC addresses) and edges (BTC transactions)
-            if address1 is not None and address2 is not None:
-                G.add_node(address1)
-                G.add_node(address2)
-                G.add_edge(address1, address2, weight=0.5)
+    df = df.sort_values(by='timestamp', ascending=True)
 
-    return G
+    return df
 
-def print_network_data(G):
-    num_nodes = G.number_of_nodes()
-    num_edges = G.number_of_edges()
-    avg_degree = np.mean([deg for node, deg in G.degree()])
-    std_dev_degree = np.std([deg for node, deg in G.degree()])
 
-    print(num_nodes, num_edges, avg_degree, std_dev_degree)
+def analyse_correlation(df):
+    correlation_matrix = df.corr()
 
-    # Visualize the network
-    nx.draw(G, with_labels=True)
+    print("Correlation Matrix:", correlation_matrix)
+
+    # Plot price, volume
+    plt.figure()
+    plt.plot(df['timestamp'], df['close'], label='close')
+    plt.plot(df['timestamp'], df['volume'], label='volume')
+    plt.plot(df['timestamp'], df['transaction_count'], label='transaction_count')
+    plt.plot(df['timestamp'], df['size'], label='size')
+    plt.legend()
+    plt.show()
 
 
 if __name__ == "__main__":
